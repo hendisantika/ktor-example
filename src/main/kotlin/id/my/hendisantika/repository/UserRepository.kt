de27c61f1e92da610232092cd4b9fc3a528424bb
hendisantika <hendisantika@yahoo.co.id>
2025-07-23 07:35:40 +0700

    feat: Add deleteUser in UserRepository🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/repository/UserRepository.kt
+++ src/main/kotlin/id/my/hendisantika/repository/UserRepository.kt
@@ -18,4 +18,5 @@ interface UserRepository {
     suspend fun user(id: Int): Player?
     suspend fun addNewUser(name: String, profilePic: String?): Player?
     suspend fun editUser(id: Int, name: String, profilePic: String?): Boolean
+    suspend fun deleteUser(id: Int): Boolean
 }
