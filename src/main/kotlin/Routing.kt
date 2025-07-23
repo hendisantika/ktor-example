2483812a45922b4837335384e4b731ae3689381c
hendisantika <hendisantika@yahoo.co.id>
2025-07-23 07:42:02 +0700

    feat: Add adduser route🫸🌀✏️📗🐧🐳

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,13 +3,23 @@ package id.my.hendisantika
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        post("/adduser") {
+            val formParameters = call.receiveParameters()
+            val name = formParameters.getOrFail("name")
+            val profilePic = formParameters.getOrFail("profilePic")
+            dao.addNewUser(name, profilePic)
+            call.respond("User added successfully")
         }
     }
 }
