c2f7a9db276d153d1aca0d6c9cb3f779eeafa235
hendisantika <hendisantika@yahoo.co.id>
2025-07-23 07:26:22 +0700

    feat: Initial project🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package id.my.hendisantika
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
