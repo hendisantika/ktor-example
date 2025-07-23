598906e42f1005016b78bc1797fae8bbc20ad7ad
hendisantika <hendisantika@yahoo.co.id>
2025-07-23 07:36:26 +0700

    feat: Add UserDaoImpl🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/repository/UserDaoImpl.kt
@@ -0,0 +1,14 @@
+package id.my.hendisantika.id.my.hendisantika.repository
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ktor-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 23/07/25
+ * Time: 07.36
+ * To change this template use File | Settings | File Templates.
+ */
+class UserDaoImpl : UserRepository
