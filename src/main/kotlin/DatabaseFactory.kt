651cffb50e10f5f682b0a529f0013e7db0f4028c
hendisantika <hendisantika@yahoo.co.id>
2025-07-23 07:32:02 +0700

    feat: Add dbQuery in DatabaseFactor🫸🌀✏️📗🐧🐳

--- src/main/kotlin/DatabaseFactory.kt
+++ src/main/kotlin/DatabaseFactory.kt
@@ -1,7 +1,9 @@
 package id.my.hendisantika
 
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
@@ -26,6 +28,9 @@ object DatabaseFactory {
         transaction(database) {
             SchemaUtils.create(Players)
         }
+
+        suspend fun <T> dbQuery(block: suspend () -> T): T =
+            newSuspendedTransaction(Dispatchers.IO) { block() }
     }
 
 
