03740aacfe3f4fc667646db178f1dbdeca416492
hendisantika <hendisantika@yahoo.co.id>
2025-07-23 07:36:38 +0700

    feat: Add allUsers in UserDaoImpl🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/repository/UserDaoImpl.kt
+++ src/main/kotlin/id/my/hendisantika/repository/UserDaoImpl.kt
@@ -1,5 +1,9 @@
 package id.my.hendisantika.id.my.hendisantika.repository
 
+import id.my.hendisantika.id.my.hendisantika.entity.Player
+import id.my.hendisantika.id.my.hendisantika.entity.Players
+import org.jetbrains.exposed.sql.selectAll
+
 /**
  * Created by IntelliJ IDEA.
  * Project : ktor-sample
@@ -11,4 +15,8 @@ package id.my.hendisantika.id.my.hendisantika.repository
  * Time: 07.36
  * To change this template use File | Settings | File Templates.
  */
-class UserDaoImpl : UserRepository
+class UserDaoImpl : UserRepository {
+    override suspend fun allUsers(): List<Player> = dbQuery {
+        Players.selectAll().map(::resultRowToArticle)
+    }
+}
